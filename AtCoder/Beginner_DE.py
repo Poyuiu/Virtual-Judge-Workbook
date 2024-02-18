01444262f0f1183813f359a901c7d3348ff94ff1
Po-Yu Juan <pyjuan91@gmail.com>
2024-02-19 01:52:39 +0800

    'update'

--- AtCoder/Beginner_DE.py
+++ AtCoder/Beginner_DE.py
@@ -1,13 +1,18 @@
-file = open("./Beginner_DE.md", "w")
+file = open("./Beginner_DE.md", "w", encoding="utf-8")
 
 # miscllaneous
 file.write(
     "According to a [tutorial](https://codeforces.com/blog/entry/66909) from Codeforces. To become an orange-rated coder, a good training method is to quickly solve the D and E problems of the AtCoder Beginner Contest. This allows oneself to compete with Japanese programmers who excel at fast coding. Therefore, let's take on this speedrun challenge.\n"
 )
 
-# from 126 to 340 (inclusive) (Feb 11th, 2024)
+# Song Yuqi
+file.write(
+    "\n\n![Alt Text](https://i.pinimg.com/originals/a2/8a/86/a28a86719db56001f029541ea53e2d27.gif)\n\n"
+)
+
+# from 126 to 341 (inclusive) (Feb 19th, 2024)
 begin = 126
-lastest = 340
+lastest = 341
 accumulated_count = 0
 
 for i in range(lastest, begin - 1, -1):
